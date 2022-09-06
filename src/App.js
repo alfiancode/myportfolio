0befdef1b3a78c9e572e1cea3b37d05573135685
alfiansm07 <Rizal.yakul.2261@outlook.co.id>
2022-09-06 23:20:41 +0700

    belajar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="h-screen">
-        <button className="btn btn-xl btn-primary rounded-none drop-shadow-none flex m-auto justi">
-          tes{" "}
-        </button>
+      <div className=" flex flex-col">
+        <div className="flex items-center justify-center">
+          <button className="w-20  btn btn-xl btn-primary rounded-none drop-shadow-none flex items-center justify-center ">
+            tes{" "}
+          </button>
+        </div>
+        <h1 className="text-4xl font-bold">alfian</h1>
+        <div className="text-5xl text-amber-300 flex w-full">muhamamd s </div>
+        <div className="w-full h-80 items-center justify-center flex">
+          <div className=" w-20 h-20 m-auto bg-slate-50 flex items-center justify-center rounded-full hover:bg-slate-300">
+            {" "}
+            haloaa
+          </div>
+        </div>
       </div>
     </>
   );
