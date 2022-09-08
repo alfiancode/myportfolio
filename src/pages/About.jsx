7bba55cb8fc867d9826c43a66df942816f90c655
alfiansm07 <34205204+alfiansm07@users.noreply.github.com>
2022-09-08 12:45:18 +0700

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     <div className="">
       {/* 1 */}
       <div className="mockup-code mx-8 mb-8 p-3 ">
-        <pre className="bg-red-300" data-prefix=">">
+        <pre className="" data-prefix=">">
           <code className="  font-thin text-3xl tracking-wide font-serif ">
             Muhammad Alfian Syah
           </code>
@@ -31,6 +31,34 @@ const About = () => {
       </div>
 
       {/* 2 */}
+      <div className="mockup-code mx-8 mb-8 p-3 ">
+        <pre className="" data-prefix=">">
+          <code className="  font-thin text-3xl tracking-wide font-serif ">
+            Tools and Technologies I use ⚙️
+          </code>
+        </pre>
+        <pre data-prefix=">" className="font-serif">
+          <code>
+            JavaScript | React | HTML5 | CSS3 | Bootstrap | Material UI |
+            Tailwind
+          </code>
+        </pre>
+        <pre data-prefix=">" className="font-serif">
+          <code className="">now living in Jakarta.Id</code>
+        </pre>
+        <pre data-prefix=">" className="font-serif">
+          <code className="">Working 9 to 5 in the tax office</code>
+        </pre>
+        <pre data-prefix=">" className="font-serif">
+          <code className="">become a front end developer at night</code>
+        </pre>
+        <pre data-prefix=">" className="font-serif">
+          <code className="">write code from heart ❤️</code>
+        </pre>
+        <pre data-prefix=">" className="text-success">
+          <code>Done!</code>
+        </pre>
+      </div>
     </div>
   );
 };
