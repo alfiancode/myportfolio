a2f6310923d35d103c723c9a1ccb544bb0871863
alfiansm07 <Rizal.yakul.2261@outlook.co.id>
2022-09-09 15:12:27 +0700

    dep new

--- src/components/contentBlog/tailwindcss.jsx
+++ src/components/contentBlog/tailwindcss.jsx
@@ -3,13 +3,7 @@ import React from "react";
 const tailwindcss = () => {
   return (
     <div className="min-h-screen bg-gray-50 py-8 flex flex-col justify-center relative overflow-hidden lg:py-12">
-      <img
-        src="/img/beams.jpg"
-        alt=""
-        className="fixed top-48 left-1/2 -translate-x-2/3 -translate-y-1/2 max-w-none"
-        width={1308}
-      />
-      <div className="absolute inset-0 bg-[url(/img/grid.svg)] bg-top [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]" />
+      <div className="absolute inset-0  bg-top [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]" />
       <div className="relative w-full px-6 py-12 bg-white shadow-xl shadow-slate-700/10 ring-1 ring-gray-900/5 md:max-w-3xl md:mx-auto lg:max-w-4xl lg:pt-16 lg:pb-28">
         <div className="max-w-prose mx-auto lg:text-lg">
           <svg className="h-6" fill="none" viewBox="0 0 297 24">
