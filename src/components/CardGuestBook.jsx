6b1bcca70797f84800e1864b53b29f3f683b5aa9
alfiansm07 <Rizal.yakul.2261@outlook.co.id>
2022-09-10 23:07:17 +0700

    Update CardGuestBook.jsx

--- src/components/CardGuestBook.jsx
+++ src/components/CardGuestBook.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 
 const CardGuestBook = ({ displayName, createdAt, comment }) => {
-  const dateCreated = createdAt?.toDate().toDateString();
-  // console.log("ini tes", dateCreated);
+  const dateCreated = createdAt?.toDate().toLocaleDateString("en-US");
+  const timeCreated = createdAt?.toDate().toLocaleTimeString();
+  // console.log("ini tes", createdAt);
   return (
     <div className="my-5">
       <div className="mb-4 w-full font-light">{comment}</div>
       <div className="">
         <div className="text-gray-600 text-sm font-extralight">
-          {displayName} |{dateCreated}
+          {displayName} |{dateCreated} {timeCreated}
         </div>
       </div>
       <div className="divider"></div>
