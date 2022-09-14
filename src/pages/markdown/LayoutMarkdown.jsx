7e4c3014991ac4b6ffacdb836a44f3a73aa5bdc2
alfiansm07 <Rizal.yakul.2261@outlook.co.id>
2022-09-14 11:35:45 +0700

    Update LayoutMarkdown.jsx

--- src/pages/markdown/LayoutMarkdown.jsx
+++ src/pages/markdown/LayoutMarkdown.jsx
@@ -22,11 +22,10 @@ const LayoutMarkdown = () => {
 
   return (
     <div>
-      <div className="min-h-screen  py-8 px-2 flex flex-col justify-center  lg:py-12 rounded-xl">
-        <div className=" inset-0 " />
-        <div className="relative w-full px-6 py-12 bg-white shadow-xl shadow-slate-700/10 ring-1 ring-gray-900/5 md:max-w-3xl md:mx-auto lg:max-w-4xl lg:pt-16 lg:pb-28">
+      <div className="px-8 py-8 px- flex flex-col justify-center  lg:py-12  ">
+        <div className="relative w-full px-6 py-12 bg-white shadow-xl rounded-xl shadow-slate-700/10 ring-1 ring-gray-900/5  md:mx-auto  lg:pt-10 lg:pb-28">
           <div className="max-w-prose mx-auto lg:text-lg prose">
-            <div className="mt-8 prose prose-pink mx-auto lg:prose-lg prose-blockquote:bg-gray-100 prose-headings:text-blue-400">
+            <div className="mt-8 prose prose-pink mx-auto lg:prose-lg prose-blockquote:bg-gray-100 prose-headings:text-blue-500">
               {/* innter HTML from get data  */}
               <div dangerouslySetInnerHTML={{ __html: data.content }} />
             </div>
