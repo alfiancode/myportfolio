de3864d8657bfbaf8476062cace461776f537632
alfiansm07 <Rizal.yakul.2261@outlook.co.id>
2022-09-14 11:30:39 +0700

    Update LayoutMarkdown.jsx

--- src/pages/markdown/LayoutMarkdown.jsx
+++ src/pages/markdown/LayoutMarkdown.jsx
@@ -22,7 +22,7 @@ const LayoutMarkdown = () => {
 
   return (
     <div>
-      <div className="min-h-screen  py-8 px-8 flex flex-col justify-center  lg:py-12 rounded-xl">
+      <div className="min-h-screen  py-8 px-2 flex flex-col justify-center  lg:py-12 rounded-xl">
         <div className=" inset-0 " />
         <div className="relative w-full px-6 py-12 bg-white shadow-xl shadow-slate-700/10 ring-1 ring-gray-900/5 md:max-w-3xl md:mx-auto lg:max-w-4xl lg:pt-16 lg:pb-28">
           <div className="max-w-prose mx-auto lg:text-lg prose">
