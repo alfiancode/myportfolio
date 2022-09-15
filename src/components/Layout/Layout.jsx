fa783f0e9f896f35b7eaab59cf6e3bb075b204fb
alfian <110095442+alfiancode@users.noreply.github.com>
2022-09-15 23:25:29 +0700

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -4,7 +4,7 @@ import Footer from "./Footer";
 const Layout = ({ children }) => {
   return (
     <div className="bg-base-200">
-      <div className="flex flex-col min-h-screen mx-auto max-w-3xl ">
+      <div className="flex flex-col min-h-screen mx-auto max-w-screen-md ">
         <Navbar />
         <div className=" flex-1 ">{children}</div>
         <Footer />
